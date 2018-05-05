f399f4dd616a0e5518f1ab3683401a32b5b3b412
Claudio E. de Oliveira <claudioed.oliveira@gmail.com>
2018-05-05 10:40:42 -0300

    Initial commit

---
+++ src/main/java/tech/claudioed/springbootforjenkins/SpringBootForJenkinsApplication.java
@@ -0,0 +1,12 @@
+package tech.claudioed.springbootforjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootForJenkinsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootForJenkinsApplication.class, args);
+  }
+}
---
+++ src/test/java/tech/claudioed/springbootforjenkins/SpringBootForJenkinsApplicationTests.java
@@ -0,0 +1,16 @@
+package tech.claudioed.springbootforjenkins;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootForJenkinsApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
